699bbbca571041b615ebedda5f29111528c099a4
unknown <ritikakumari2910@gmail.com>
2026-01-05 16:46:38 +0530

    updated

--- server/config/db.js
+++ server/config/db.js
@@ -18,6 +18,7 @@ const pool = mysql.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME, // Use DB_NAME from .env
+    port: process.env.DB_PORT, // Critical for Aiven/Cloud DBs using non-standard ports
     multipleStatements: true,
 });
 
